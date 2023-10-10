47735a93fec012d533e042ac082229cdd9decc77
estefanynovika <146912966+estefanynovika@users.noreply.github.com>
2023-10-10 20:35:29 +0700

    Create operator_aritmatika1.c

---
+++ operator_aritmatika1.c
@@ -0,0 +1,15 @@
+//operator_aritmatika.c
+#include <stdio.h>
+void main(){
+    int a, b, c;
+
+    printf("inputkan nilai a: ");
+    scanf("%i", &a);
+
+    printf("inputkan nilai b: ");
+    scanf("%i", &b);
+
+    c = a + b;
+
+    printf("Hasil a + b: %i", c);
+}
