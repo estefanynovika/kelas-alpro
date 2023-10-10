451b7cd12ec6c22e3147192343b82a6da7dd0eef
estefanynovika <146912966+estefanynovika@users.noreply.github.com>
2023-10-10 20:36:16 +0700

    Create operator_aritmatika2.c

---
+++ operator_aritmatika2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// operator_aritmatika.c
+void main(){
+
+    int a, b;
+
+    printf("inputkan nilai a: ");
+    scanf("%i", &a);
+
+    printf("inputkan nilai b: ");
+    scanf("%i", &b);
+
+    printf("Hasil a + b: %i\n", a + b);
+    printf("Hasil a - b: %i\n", a - b);
+    printf("Hasil a * b: %i\n", a * b);
+    printf("Hasil a / b: %i\n", a / b);
+    printf("Hasil a % b: %i\n", a % b);
+
+}
