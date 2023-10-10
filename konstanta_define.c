39301f33a8051ff5f7a7016cebb218d3c2438d98
estefanynovika <146912966+estefanynovika@users.noreply.github.com>
2023-10-10 20:33:31 +0700

    Create konstanta_define.c

---
+++ konstanta_define.c
@@ -0,0 +1,12 @@
+// konstanta pada C
+// define
+#include <stdio.h>
+#define SEPULUH 10
+#define VERSI 4.5
+#define JENIS_KELAMIN 'L'
+
+void main(){
+    printf("isi konstanta SEPULUH adalah %i\n", SEPULUH);
+    printf("isi konstanta VERSI adalah %f\n", VERSI);
+    printf("isi kostanta JENIS_KELAMIN adalah %c\n", JENIS_KELAMIN);
+}
