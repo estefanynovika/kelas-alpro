e3bf30ab0079f6f4dfd9050e58bcc65628ab6cf3
estefanynovika <146912966+estefanynovika@users.noreply.github.com>
2023-10-10 20:25:17 +0700

    Create scanf_1.c

---
+++ scanf_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    char name[20], web_address[30];
+
+    printf("Nama: ");
+    scanf("%s", &name);
+
+    printf("Alamat web: ");
+    scanf("%s", &web_address);
+
+    printf("\n------------------------------\n");
+    printf("Nama yang diinputkan: %s\n", name);
+    printf("Alamat Web yang diinputkan: %s\n", web_address);
+    return 0;
+}
